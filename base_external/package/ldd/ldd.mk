196f7b19f4df3c5605fb1e426e9f04ed9b0d31c3
marco <marcoronk@gmail.com>
2024-12-21 13:07:07 +0100

    ass7-1

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,28 @@
+##############################################################
+#
+# LDD
+#
+##############################################################
+$(info    Start Build LDD)
+LDD_VERSION = 'e536ebeb8fedbb58e625691a779916850e96c61c'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-marcoronk.git'
+           
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = misc-modules/ scull/
+
+
+define LDD_INSTALL_TARGET_CMDS
+     $(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/misc-modules/faulty.ko $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/sleepy.ko $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/scull/scull.ko $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
+$(info    End Build LDD)
