99e46697400adb95cdb70aec759e099924492357
marco <marcoronk@gmail.com>
2024-12-26 21:49:07 +0100

    daspararsi

--- base_external/package/chardriver/chardriver.mk
+++ base_external/package/chardriver/chardriver.mk
@@ -3,21 +3,21 @@
 # CHARDRIVER
 #
 ##############################################################
-$(info    Start Build CHARDRIVERDRIVER)
-CHARDRIVER_VERSION = 'e536ebeb8fedbb58e625691a779916850e96c61c'
-CHARDRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-marcoronk.git'         
+$(info    Start Build CHARDRIVER)
+CHARDRIVER_VERSION = 'e23c5a9b68b76a6682ae00a8ef0ab63949bc0fae'
+CHARDRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-marcoronk.git'
 CHARDRIVER_SITE_METHOD = git
 CHARDRIVER_GIT_SUBMODULES = YES
-CHARDRIVER_MODULE_SUBDIRS = chardriver/
-#CHARDRIVER_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+CHARDRIVER_MODULE_SUBDIRS = aesd-char-driver/
 
 define CHARDRIVER_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0755 $(@D)/chardriver/aesdchar.ko $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -m 0755 $(@D)/chardriver/aesdchar_load $(TARGET_DIR)/usr/bin/
-    $(INSTALL) -m 0755 $(@D)/chardriver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
 endef 
 
 
 $(eval $(generic-package))
 $(eval $(kernel-module))
-$(info    End Build CHARDRIVERDRIVER)
+$(info    End Build CHARDRIVER)
+
